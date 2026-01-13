3b2d17ef8a4f40e5aa565bde79900fa1825f5996
Devin Ford <devin.ford4263@gmail.com>
2026-01-12 19:08:40 -0600

    ready to deploy (I hope)

--- Fish_Alchemy_Data/main.py
+++ Fish_Alchemy_Data/main.py
@@ -42,7 +42,8 @@ app.mount("/media", StaticFiles(directory="media"), name="media")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:4173", "http://localhost:4173", "https://discord.com", "https://media.discordapp.net"],
+    # allow_origins=["http://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:4173", "http://localhost:4173", "https://discord.com", "https://media.discordapp.net"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
