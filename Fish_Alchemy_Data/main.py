e0ddd8f2b82fd811558626f2354b82521b32f80b
Devin Ford <devin.ford4263@gmail.com>
2026-01-13 00:01:08 -0600

    FUCK

--- Fish_Alchemy_Data/main.py
+++ Fish_Alchemy_Data/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from requests import HTTPError
 
